0ac8096e4eb5d1671694956490615457e1f74c05
lucaschfonseca <lucaschfonseca@gmail.com>
2020-03-02 09:56:51 -0300

    initial commit

---
+++ cmake-build-debug/CMakeFiles/Breadth_first_search.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "Breadth_first_search.exe"
+  "Breadth_first_search.exe.manifest"
+  "Breadth_first_search.pdb"
+  "CMakeFiles/Breadth_first_search.dir/main.c.obj"
+  "libBreadth_first_search.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Breadth_first_search.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
